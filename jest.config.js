9b3e131a197aeef131c5ce4d420d0570a74a561c
Fernando Ramirez <f.e.ramirez94@gmail.com>
2022-09-08 23:01:50 +0200

    Setup jest testing, eslint and prettier

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  testMatch: ['**/*.test.*', '**/*.spec.*'],
+  moduleNameMapper: {
+    '^.+.(css|style|less|sass|scss|png|jpg|ttf|woff|woff2)$': '<rootDir>/config/fileStub.js',
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^.+\\.(css|less)$': '<rootDir>/config/CSSStub.js',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  // setupFiles: ['./jest-global-mocks.js', './setup-env-vars.js'],
+  setupFilesAfterEnv: ['./jest-console-error-handling.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.(js|ts|tsx)',
+    '!<rootDir>/src/**/*.stories.(js|ts|tsx)', // exclude sb stories from coverage analysis
+  ],
+  reporters: ['default', 'jest-junit'],
+  // testEnvironment: 'jsdom',
+  coverageReporters: ['lcov', 'cobertura', 'text'],
+}
