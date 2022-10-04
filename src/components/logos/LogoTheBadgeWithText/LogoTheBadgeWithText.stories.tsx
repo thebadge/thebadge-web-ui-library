311cff81fa5ff3ee7e3227448f21a6edc79f3829
Fernando Ramirez <f.e.ramirez94@gmail.com>
2022-10-04 18:45:15 +0200

    fix lint errors

--- src/components/logos/LogoTheBadgeWithText/LogoTheBadgeWithText.stories.tsx
+++ src/components/logos/LogoTheBadgeWithText/LogoTheBadgeWithText.stories.tsx
@@ -3,30 +3,28 @@ import { Meta } from '@storybook/react/types-6-0'
 import { Story } from '@storybook/react'
 import { LogoTheBadgeWithText } from './LogoTheBadgeWithText'
 import colors from '@assets/scss/variables/_color.variables.module.scss'
-import gradients from '*.scss'
-import { BackgroundGradient0 } from '../../layout/BackgroundGradient/BackgroundGradient.stories'
 
 export default {
   title: 'Components/Logos/LogoTheBadgeWithText',
   component: LogoTheBadgeWithText,
 } as Meta
 
-const Template: Story<{color: string}> = (args) => {
+const Template: Story<{ color: string }> = (args) => {
   return (
-    <div style={{background: args.color }}>
+    <div style={{ background: args.color }}>
       <LogoTheBadgeWithText />
     </div>
   )
 }
 
 export const WithBlackBackground = Template.bind({})
-WithBlackBackground.args = {color: 'black'}
+WithBlackBackground.args = { color: 'black' }
 
 export const WithSecondaryBlueBackground = Template.bind({})
-WithSecondaryBlueBackground.args = {color: colors.secondaryBlue}
+WithSecondaryBlueBackground.args = { color: colors.secondaryBlue }
 
 export const WithLightBlueBackground = Template.bind({})
-WithLightBlueBackground.args = {color: colors.lightBlue}
+WithLightBlueBackground.args = { color: colors.lightBlue }
 
 export const WithPrimaryPinkBackground = Template.bind({})
-WithPrimaryPinkBackground.args = {color: colors.primaryPink}
+WithPrimaryPinkBackground.args = { color: colors.primaryPink }
