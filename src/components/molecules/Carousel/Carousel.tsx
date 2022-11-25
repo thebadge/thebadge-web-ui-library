1db761cd0d87fd5d9fd99507cc84a4bf1aa974af
Federico Madoery <fede.madoery@hotmail.com>
2022-11-25 16:15:18 -0300

    Add comments to the carousel functions

--- src/components/molecules/Carousel/Carousel.tsx
+++ src/components/molecules/Carousel/Carousel.tsx
@@ -26,9 +26,12 @@ export const Carousel = (props: CarouselProps = defaultCarouselProps) => {
 
   function getHelperData() {
     const cellCount = props.elements.length
+    // We need to think the carousel as a big circle, each cell is a partition on that cirle,
+    // so the theta is the angle size or each partition.
     const theta = 360 / cellCount
     const cellSize = props.elementWidth + 16
 
+    // This is the radios of the circle, we use the tangen equatio to do so
     const radius = Math.round(cellSize / 2 / Math.tan(Math.PI / cellCount))
 
     return { cellCount, theta, radius, cellSize }
@@ -36,14 +39,22 @@ export const Carousel = (props: CarouselProps = defaultCarouselProps) => {
 
   function rotateCarousel() {
     const { theta, radius } = getHelperData()
+    // We need to know the angle of the new selected cell, so we need to multiply the Theta by the
+    // index of the selected cell, bc of that we have a minus one
     const angle = theta * selectedCell * -1
     const carousel = carouselRef.current
     if (!carousel) return
+    // Think it as circle thats rotate on the center a real carousel.
+    // We apply a translate on Z and Y, to "rotate" the circle and put the cell on the front.
+    // Rotate Y - Rotates an element around the horizontal Y axis
+    // Translaze Z - Positions it along the Z axis, which runs front to back in 3D space.
     carousel.style.transform = `translateZ(${-radius}px) rotateY(${angle}deg)`
   }
 
   function changeCarousel() {
     const { cellCount, theta, radius } = getHelperData()
+    // We need to go on each element and put it over the circumference of our virtual circle
+    // To do that, we need to rotate each element to the corresponding angle.
     for (let i = 0; i < elementRefs.length; i++) {
       let cell = elementRefs[i].current
       if (!cell) return
