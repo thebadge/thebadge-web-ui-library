28af758b6a47ea29a7f73fd96833bd4b390014e9
Fernando Ramirez <f.e.ramirez94@gmail.com>
2022-12-01 16:48:46 +0100

    fix lint errors

--- src/components/molecules/Stepper/Stepper.stories.tsx
+++ src/components/molecules/Stepper/Stepper.stories.tsx
@@ -22,23 +22,23 @@ StepperPreview.args = {
   color: 'green',
   title: 'off-chain HOW IT WORKS',
   elements: [
-    <Typography fontWeight={500} fontSize={18} fontFamily="Mulish">
+    <Typography key={0} fontWeight={500} fontSize={18} fontFamily="Mulish">
       Select the type of badge you are interested in. Each badge will require the submission of proof or evidence to
       certify you have the right to claim it. An example of this type of badge could be to claim a Twitter account.
     </Typography>,
-    <Typography fontWeight={500} fontSize={18} fontFamily="Mulish">
+    <Typography key={1} fontWeight={500} fontSize={18} fontFamily="Mulish">
       Prepare and submit the evidence. Each type of badge will request to accomplish some specific tasks. For example, a
       tweet containing your wallet address.
     </Typography>,
-    <Typography fontWeight={500} fontSize={18} fontFamily="Mulish">
-      Your submission will have to pass through Kleros's curation process. Where the community will have a few days to
-      analyze the evidence you have submitted.
+    <Typography key={2} fontWeight={500} fontSize={18} fontFamily="Mulish">
+      Your submission will have to pass through Kleros`&apos;`s curation process. Where the community will have a few
+      days to analyze the evidence you have submitted.
     </Typography>,
-    <Box display="flex" flexDirection="row" alignContent="center">
+    <Box key={3} display="flex" flexDirection="row" alignContent="center">
       <Box width={90} height={120} bgcolor="black" borderRadius={1} marginRight={2} />
       <Typography fontWeight={500} fontSize={18} fontFamily="Mulish">
-        Your submission will have to pass through Kleros's curation process. Where the community will have a few days to
-        analyze the evidence you have submitted.
+        Your submission will have to pass through Kleros`&apos;`s curation process. Where the community will have a few
+        days to analyze the evidence you have submitted.
       </Typography>
     </Box>,
   ],
--- src/index.ts
+++ src/index.ts
@@ -29,5 +29,5 @@ export {
   BadgeCategories,
   BadgeTypesSupported,
   Carousel,
-  Stepper
+  Stepper,
 }
