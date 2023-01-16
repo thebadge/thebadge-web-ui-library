7de11595a070155e2437ca2910afc9472cc038fd
Federico Madoery <fede.madoery@hotmail.com>
2023-01-16 10:10:56 -0300

    Export type props (#32)
    Export type props

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,15 @@ import breakpoints from '@assets/scss/variables/_breakpoint.variables.module.scs
 import colors from '@assets/scss/variables/_color.variables.module.scss'
 import fonts from '@assets/scss/variables/_fonts.variables.module.scss'
 import gradients from '@assets/scss/variables/_gradient.variables.module.scss'
+import { BadgePreviewProps } from '@components/atoms/BadgePreview/BadgePreviewProps'
 
 // components
 import { BadgePreview } from '@components/atoms/BadgePreview/v1/BadgePreview'
 import { BadgePreviewV2 } from '@components/atoms/BadgePreview/v2/BadgePreview'
 import { Button } from '@components/atoms/Button/v1/Button'
 import { ButtonV2 } from '@components/atoms/Button/v2/Button'
-import { NavigationHeader } from '@components/atoms/NavigationHeader/NavigationHeader'
-import { SpinningArrow } from '@components/atoms/SpinningArrow/SpinningArrow'
+import { NavigationHeader, NavigationHeaderProps } from '@components/atoms/NavigationHeader/NavigationHeader'
+import { SpinningArrow, SpinningArrowProps } from '@components/atoms/SpinningArrow/SpinningArrow'
 import { IconBadge } from '@components/icons/IconBadge/IconBadge'
 import { IconCurator } from '@components/icons/IconCurator/IconCurator'
 import { IconDecentralized } from '@components/icons/IconDecentralized/IconDecentralized'
@@ -25,13 +26,14 @@ import { IconGithub } from '@components/icons/IconGithub/IconGithub'
 import { IconMedium } from '@components/icons/IconMedium/IconMedium'
 import { IconTwitter } from '@components/icons/IconTwitter/IconTwitter'
 import { BackgroundGradient } from '@components/layout/BackgroundGradient/BackgroundGradient'
-import { BoxGradient } from '@components/layout/BoxGradient/BoxGradient'
-import { SectionLayout } from '@components/layout/SectionLayout/SectionLayout'
+import { BoxGradient, BoxGradientProps } from '@components/layout/BoxGradient/BoxGradient'
+import { SectionLayout, SectionLayoutProps } from '@components/layout/SectionLayout/SectionLayout'
+import { LogoProps } from '@components/logos/LogoProps'
 import { LogoTheBadge } from '@components/logos/LogoTheBadge/LogoTheBadge'
 import { LogoTheBadgeWithText } from '@components/logos/LogoTheBadgeWithText/LogoTheBadgeWithText'
 import { Carousel } from '@components/molecules/Carousel/Carousel'
-import { MultiBadgePreview } from '@components/molecules/MultiBadgePreview/MultiBadgePreview'
-import { Stepper } from '@components/molecules/Stepper/Stepper'
+import { MultiBadgePreview, MultiBadgePreviewProps } from '@components/molecules/MultiBadgePreview/MultiBadgePreview'
+import { Stepper, StepperProps } from '@components/molecules/Stepper/Stepper'
 
 export {
   defaultTheme,
@@ -63,4 +65,14 @@ export {
   Stepper,
   SpinningArrow,
 }
-export type { TBColor }
+export type {
+  TBColor,
+  StepperProps,
+  MultiBadgePreviewProps,
+  LogoProps,
+  SectionLayoutProps,
+  BoxGradientProps,
+  SpinningArrowProps,
+  NavigationHeaderProps,
+  BadgePreviewProps,
+}
