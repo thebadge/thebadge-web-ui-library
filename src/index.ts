5b06a5fa021be44875877946b64d22f5747e9494
Agustin Pane <agustin.pane@gmail.com>
2023-05-15 13:42:10 +0200

    Add missing types to export

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ import { LogoProps } from '@components/logos/LogoProps'
 import { LogoTheBadge } from '@components/logos/LogoTheBadge/LogoTheBadge'
 import { LogoTheBadgeWithText } from '@components/logos/LogoTheBadgeWithText/LogoTheBadgeWithText'
 import { Carousel } from '@components/molecules/Carousel/Carousel'
-import { Roadmap } from '@components/molecules/Roadmap/Roadmap'
+import { Roadmap, RoadmapItem, RoadmapProps } from '@components/molecules/Roadmap/Roadmap'
 import { MultiBadgePreview, MultiBadgePreviewProps } from '@components/molecules/MultiBadgePreview/MultiBadgePreview'
 import {
   ResizedBadgePreviewsList,
@@ -92,4 +92,6 @@ export type {
   NavigationHeaderProps,
   BadgePreviewProps,
   ResizedBadgePreviewsListProps,
+  RoadmapProps,
+  RoadmapItem,
 }
