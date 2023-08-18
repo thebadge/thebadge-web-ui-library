138eb9e8a4d74736eb187b2673f735fdaa6f00eb
Federico Madoery <fede.madoery@hotmail.com>
2023-08-18 08:31:21 -0300

    Improve badge spaces

--- src/components/atoms/BadgePreview/v2/BadgePreview.tsx
+++ src/components/atoms/BadgePreview/v2/BadgePreview.tsx
@@ -5,7 +5,7 @@ import { Box, styled, Typography } from '@mui/material'
 import React from 'react'
 import QRCode from 'react-qr-code'
 import './badgePreview.scss'
-
+import defaultBackground from '@assets/svgs/badgeBackground.svg'
 const defaultBackgroundUrl =
   'https://images.unsplash.com/photo-1566041510639-8d95a2490bfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=678&q=80'
 
@@ -20,32 +20,44 @@ const defaultValuesForBadgePreviewProps = {
   animationEffects: ['wobble', 'grow'] as BadgePreviewEffects[],
 }
 
-const BadgePreviewBox = styled(Box, { shouldForwardProp: (propName) => propName !== 'size' })<{ size?: number }>(
-  ({ size = 320 }) => ({
-    width: size,
-    height: size * 1.6,
-  })
-)
+const BadgePreviewBox = styled(Box, { shouldForwardProp: (propName) => propName !== 'size' })<{
+  size: { width: number; height: number }
+}>(({ size }) => ({
+  width: size.width,
+  height: size.height,
+}))
 
 export const BadgePreview = (props: BadgePreviewProps = defaultValuesForBadgePreviewProps) => {
   const badgeSize = () => {
     const badgePreviewPropsSize = props.size
     switch (badgePreviewPropsSize) {
       case 'small':
-        return 200
+        return { width: 200, height: 328 }
       case 'medium':
-        return 300
+        return { width: 300, height: 504 }
       case 'large':
-        return 400
+        return { width: 400, height: 655 }
       case 'x-large':
-        return 500
+        return { width: 400, height: 655 }
       default:
-        return 300
+        return { width: 200, height: 328 }
     }
   }
 
   const badgeImageSize = () => {
-    return badgeSize() / 2
+    const badgePreviewPropsSize = props.size
+    switch (badgePreviewPropsSize) {
+      case 'small':
+        return { width: 160, height: 160 }
+      case 'medium':
+        return { width: 236, height: 236 }
+      case 'large':
+        return { width: 320, height: 320 }
+      case 'x-large':
+        return { width: 320, height: 320 }
+      default:
+        return { width: 160, height: 160 }
+    }
   }
 
   const badgeLogoSize = () => {
@@ -108,7 +120,7 @@ export const BadgePreview = (props: BadgePreviewProps = defaultValuesForBadgePre
 
   const badgeTitleMaxLines = () => {
     const size = badgeSize()
-    return size > 500 ? 2 : 1
+    return size.width > 500 ? 2 : 1
   }
 
   const animationEffectClasses = () => {
@@ -130,14 +142,19 @@ export const BadgePreview = (props: BadgePreviewProps = defaultValuesForBadgePre
           `badge-previewV2__container--${props.textContrast ?? 'light'}`,
         ].join(' ')}
       >
-        <img
-          className={`badge-previewV2__container--backgroundImage`}
-          src={`${props.badgeBackgroundUrl ? props.badgeBackgroundUrl : defaultBackgroundUrl}`}
-          alt="Badge Background"
-        />
+        {props.badgeBackgroundUrl ? (
+          <img
+            className={`badge-previewV2__container--backgroundImage`}
+            src={`${props.badgeBackgroundUrl ? props.badgeBackgroundUrl : defaultBackground}`}
+            alt="Badge Background"
+          />
+        ) : (
+          <div className={`badge-previewV2__container--backgroundImage`} style={{ background: 'green' }} />
+        )}
         <div
           className={[
             `badge-previewV2__container__shadow`,
+            `badge-previewV2__container__shadow--${props.size}`,
             `badge-previewV2__container__shadow--${props.textContrast ?? 'light'}`,
           ].join(' ')}
         />
@@ -164,10 +181,11 @@ export const BadgePreview = (props: BadgePreviewProps = defaultValuesForBadgePre
             <img
               src={props.imageUrl}
               alt="Badge image"
+              style={{ background: 'grey' }}
               className={[`badge-previewV2__image`, `badge-previewV2__image--${props.size}`].join(' ')}
             />
           ) : (
-            <LogoTheBadgeWithText size={badgeImageSize()} />
+            <LogoTheBadgeWithText size={badgeImageSize().width} />
           )}
         </div>
         {/* Badge Content - Tittle - Category - Description */}
