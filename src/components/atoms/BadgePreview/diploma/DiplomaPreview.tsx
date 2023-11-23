47f810524f2cdd97a803fc8fd8739b62e56e9556
Fernando Ramirez <f.e.ramirez94@gmail.com>
2023-11-23 19:26:44 +0100

    Remove unnecessary images

--- src/components/atoms/BadgePreview/diploma/DiplomaPreview.tsx
+++ src/components/atoms/BadgePreview/diploma/DiplomaPreview.tsx
@@ -5,8 +5,6 @@ import { Box, Divider, styled, Tooltip } from '@mui/material'
 import React from 'react'
 import QRCode from 'react-qr-code'
 import './diplomaPreview.scss'
-import whiteBackground from '@assets/svgs/whiteBackground.svg'
-import blueBackground from '@assets/svgs/blueBackground.svg'
 import { CertificateWaterMark } from '@components/icons/CertificateWaterMark/CertificateWaterMark'
 import { LogoTheBadge } from '@components/logos/LogoTheBadge/LogoTheBadge'
 import { IconVerified } from '@components/icons/IconVerified/IconVerified'
@@ -100,7 +98,7 @@ export const DiplomaPreview = (props: DiplomaPreviewProps) => {
         ) : (
           <img
             className={`diploma-preview__content--backgroundImage`}
-            src={`${whiteBackground}`}
+            src={"https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
             alt="Diploma Background"
             style={{ maxWidth: 'none', width: 'auto' }}
           />
@@ -174,7 +172,7 @@ export const DiplomaPreview = (props: DiplomaPreviewProps) => {
         ) : (
           <img
             className={`diploma-preview__content--backgroundImage`}
-            src={`${blueBackground}`}
+            src={"https://images.unsplash.com/photo-1614850523011-8f49ffc73908?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
             alt="Diploma Background"
             style={{ maxWidth: 'none', width: 'auto' }}
           />
